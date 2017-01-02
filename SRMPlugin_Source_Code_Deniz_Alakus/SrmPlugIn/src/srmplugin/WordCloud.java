3cf1e41f1c6a058e275b7420abd21286ce926fb0
Yannick Kaupe <yannick.kaupe@gmail.com>
2017-01-02 19:13:07 +0100

    Extracted Filenames from porperty paths
    Stored only the filenames without pathinfo in List for future
    Wordcloud generation. Will be used for counting occurences and
    as Label text. Word count will determine fontsize.
    
    Signed-off-by: Yannick Kaupe <yannick.kaupe@gmail.com>

--- SRMPlugin_Source_Code_Deniz_Alakus/SrmPlugIn/src/srmplugin/WordCloud.java
+++ SRMPlugin_Source_Code_Deniz_Alakus/SrmPlugIn/src/srmplugin/WordCloud.java
@@ -13,8 +13,10 @@ import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.swing.JLabel;
 
@@ -23,12 +25,14 @@ import srmprocess.DBConnection;
 
 public class WordCloud extends ViewPart {
 	public static final String ID = "SrmPlugIn.WordCloud";
+	List<String> filenames = new ArrayList<String>();
 
 	// the listener we register with the selection service
 	private ISelectionListener listener = new ISelectionListener() {
 		public void selectionChanged(IWorkbenchPart sourcepart, ISelection selection) {
 			// we ignore our own selections
 			if (sourcepart != WordCloud.this) {
+				filenames.clear();
 				determineThePathOfTheSelectedFileAndSendIt(sourcepart, selection);
 			}
 		}
@@ -47,13 +51,55 @@ public class WordCloud extends ViewPart {
 		System.out.println("Created Word Cloud Table");
 	}
 
+	
+	private void addFilenameToList(String filepath) {
+		if (((filepath) != (null))
+				&& (filepath).length() > 0
+				&& !(filepath).equals(" ")
+				&& filepath.contains("/")
+				&& filepath.contains(".")) {
+
+			String filename = getFileName(filepath);
+			filenames.add(filename);
+		}
+		
+	}
+	
+	/**
+	 * Extracts the filename from a given property path string
+	 * 
+	 * @param pathString
+	 * @return
+	 */
+	public String getFileName(String pathString) {
+		String filename = "!~UNEXPECTED ERROR~!";
+
+		String path = pathString;
+		int lastDash;
+		int lastDot;
+		try {
+
+			if (path.contains(".") && path.contains("/")) {
+				lastDash = path.lastIndexOf("/");
+				lastDot = path.lastIndexOf(".");
+				if (lastDot > lastDash) {
+					filename = path.substring(lastDash + 1, lastDot);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return filename;
+	}
+
 	@Override
 	public void createPartControl(Composite parent) {
 		// registriere Listener
 		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(listener);
-		
+
 		BundleContext ctx = FrameworkUtil.getBundle(WordCloud.class).getBundleContext();
-		
+
 		Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED);
 		java.awt.Frame frame = SWT_AWT.new_Frame(swtAwtComponent);
 		javax.swing.JPanel panel = new javax.swing.JPanel();
@@ -61,36 +107,52 @@ public class WordCloud extends ViewPart {
 		EventHandler handler = event -> {
 
 			if (Communication.control) {
-				//TODO: Clear the panel
-				
+				// TODO: Clear the panel
+
 				System.out.println("1");
-				
+
 			} else {
 
 				if (parent.getDisplay().getThread() == Thread.currentThread()) {
 
-					//add clickable labels for filenames tied to filepaths via some ID
+					// add clickable labels for filenames tied to filepaths via
+					// some ID
+
+					// get file name of path
+					// src/main/java/de/MainClass.java -> MainClass
+
+					System.out.println("### DEBUG ### " + ((String) event.getProperty("file")));
+
+					addFilenameToList((String) event.getProperty("file"));
 					
-					System.out.println(event.getProperty("file"));
+
+					for (String file : filenames) {
+						System.out.println(file);
+
+					}
+					System.out.println("################");
+					System.out.println("################");
+
 				} else {
-					parent.getDisplay().syncExec( () -> System.out.println("3") );
+					parent.getDisplay().syncExec(() -> System.out.println("3"));
 				}
 			}
 		};
-		
+
 		Dictionary<String, String> properties = new Hashtable<String, String>();
 		properties.put(EventConstants.EVENT_TOPIC, "viewcommunicationfile/*");
 		ctx.registerService(EventHandler.class, handler, properties);
-		
+
 		OnStart();
-		
-		
+
 		panel.setForeground(Color.white);
 
 		frame.add(panel);
 		frame.setForeground(Color.white);
 
 	}
+
+	
 
 	@Override
 	public void setFocus() {
